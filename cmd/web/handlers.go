ebe180efc71fb7a9bda07be34a59aef9805ca99f
Kenneth Fan <kf6999@rit.edu>
2022-02-02 11:11:48 -0500

    Template actions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,26 +22,27 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    for _, snippet := range s {
-        fmt.Fprintf(w, "%v\n", snippet)
+    // Create an instance of a templateData struct holding the slice of
+    // snippets.
+    data := &templateData{Snippets: s}
+
+    files := []string{
+        "./ui/html/home.page.tmpl",
+        "./ui/html/base.layout.tmpl",
+        "./ui/html/footer.partial.tmpl",
     }
 
-    // files := []string{
-    //     "./ui/html/home.page.tmpl",
-    //     "./ui/html/base.layout.tmpl",
-    //     "./ui/html/footer.partial.tmpl",
-    // }
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
 
-    // ts, err := template.ParseFiles(files...)
-    // if err != nil {
-    //     app.serverError(w, err)
-    //     return
-    // }
-
-    // err = ts.Execute(w, nil)
-    // if err != nil {
-    //     app.serverError(w, err)
-    // }
+    // Pass in the templateData struct when executing the template.
+    err = ts.Execute(w, data)
+    if err != nil {
+        app.serverError(w, err)
+    }
 }
 
 
