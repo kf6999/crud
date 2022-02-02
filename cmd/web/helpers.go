d79323f2622b1525017bb9053d8710cf33851ceb
Kenneth Fan <kf6999@rit.edu>
2022-02-02 11:33:31 -0500

    Common dynamic data

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 // The serverError helper writes an error message and stack trace to the errorLog,
@@ -30,6 +31,14 @@ func (app *application) notFound(w http.ResponseWriter) {
     app.clientError(w, http.StatusNotFound)
 }
 
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+    if td == nil {
+        td = &templateData{}
+    }
+    td.CurrentYear = time.Now().Year()
+    return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
     ts, ok := app.templateCache[name]
     if !ok {
@@ -37,21 +46,16 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
         return
     }
 
-    // Initialize a new buffer.
     buf := new(bytes.Buffer)
 
-    // Write the template to the buffer, instead of straight to the
-    // http.ResponseWriter. If there's an error, call our serverError helper and then
-    // return.
-    err := ts.Execute(buf, td)
+    // Execute the template set, passing the dynamic data with the current
+    // year injected.
+    err := ts.Execute(buf, app.addDefaultData(td, r))
     if err != nil {
         app.serverError(w, err)
         return
     }
 
-    // Write the contents of the buffer to the http.ResponseWriter. Again, this
-    // is another time where we pass our http.ResponseWriter to a function that
-    // takes an io.Writer.
     buf.WriteTo(w)
 }
 
