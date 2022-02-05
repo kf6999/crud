2db7bdf2cc9ab7a880b223d9d2c042c87e51188c
Kenneth Fan <kf6999@rit.edu>
2022-02-04 23:05:04 -0500

    Add routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,16 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/bmizerany/pat" // New import
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", app.home)
-    mux.HandleFunc("/snippet", app.showSnippet)
-    mux.HandleFunc("/snippet/create", app.createSnippet)
+    standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+    mux := pat.New()
+    mux.Get("/", http.HandlerFunc(app.home))
+    mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
+    mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
+    mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet)) // Moved down
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+    mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
-    // Wrap the existing chain with the recoverPanic middleware.
-    return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+    return standardMiddleware.Then(mux)
 }
